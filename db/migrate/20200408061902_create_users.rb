fd5cbd0787dc69db825ce17d527ed61321dedd2c
my_name <my_name@example.com>
2020-04-08 16:30:08 +0900

    first commit

---
+++ db/migrate/20200408061902_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :mail
+      t.string :password_digest
+      t.string :image
+      t.string :birthday
+      t.text :introduce
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/users.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :user do
+    name { "MyString" }
+    mail { "MyString" }
+    password_digest { "MyString" }
+    image { "MyString" }
+    birthday { "MyString" }
+    introduce { "MyText" }
+  end
+end
